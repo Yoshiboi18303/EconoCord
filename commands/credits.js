6edf30d54dd34d45d2522cdf166a9b245839b585
Yoshiboi18303 <yoshiboi18303.t@gmail.com>
2021-09-17 20:08:22 +0000

    New Command!

---
+++ commands/credits.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports.help = {
+  name: 'credits',
+  description: 'Shows the credits of who made EconoCord in an embed!'
+}
+
+module.exports.run = async (bot, cmd, args) => {
+  var client = bot;
+
+  const owner = client.users.fetch("697414293712273408")
+  const co_owner = client.users.fetch("242734840829575169")
+
+  const credits_embed = new MessageEmbed()
+    .setColor("RANDOM")
+    .setTitle(client.user.username + " Credits")
+    .addFields([
+      {
+        name: 'Owner',
+        value: `${owner.username}`
+      },
+      {
+        name: 'Co Owner',
+        value: `${co_owner.username}`
+      }
+    ])
+  cmd.reply({
+    embeds: [
+      credits_embed
+    ]
+  })
+}
